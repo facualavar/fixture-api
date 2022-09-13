3845008f4fe8a77acdf9f7d032120ba802756c1d
Facundo Alavar <facundo.alavar@enviopack.com>
2022-09-12 22:34:20 -0300

    Add action getGames and postResults to GroupController

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\GroupService;
+use App\Models\Game;
 use App\Models\Group;
 use Illuminate\Http\Request;
 
@@ -17,7 +19,22 @@ class GroupController extends Controller
 
     public function show(int $id)
     {
-        $group = Group::with('teams')->find($id);
+        $group = Group::with('teams', 'games.matchday', 'games.team1', 'games.team2')->find($id);
         return response()->json($group, 200);
+    }
+
+    public function games(int $id)
+    {
+        $games = Game::with('team1', 'team2', 'group', 'stage', 'matchday')->where('group_id', $id)->get();
+        return response()->json($games, 200);
+    }
+
+    public function results(int $id, Request $request)
+    {
+        $results      = $request->all();
+        $group        = Group::find($id);
+        $groupService = new GroupService($group);
+        $response     = $groupService->saveResults($results);
+        return response()->json($response, 200);
     }
 }
