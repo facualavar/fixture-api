dc40dbba304536994bbd325f60650afd5ba51542
Facundo Alavar <facundo.alavar@enviopack.com>
2022-09-13 01:04:35 -0300

    Add Auth Controller and routes login-register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validateData = $request->validate([
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name'     => $validateData['name'],
+            'email'    => $validateData['email'],
+            'password' => Hash::make($validateData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'Bearer'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Login failed'
+            ], 401);
+
+            $request->session()->regenerate();
+            return response("You are logged in!", 200);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'Bearer'
+        ]);
+    }
+
+    public function infoUser(Request $request)
+    {
+        return $request->user();
+    }
+}
