6811677d7533e93c17f033b69693d0d54d935ace
Facundo Alavar <facundo.alavar@enviopack.com>
2022-09-12 22:32:53 -0300

    Add Model Result

---
+++ database/migrations/2022_09_13_001932_create_results_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('game_id');
+            $table->integer('goals_team_1')->nullable();
+            $table->integer('goals_team_2')->nullable();
+            $table->unsignedBigInteger('team_winner')->nullable();
+            $table->timestamps();
+
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->foreign('team_winner')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+};
