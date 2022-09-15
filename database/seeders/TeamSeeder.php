599fe00f1439bde0d67b658dffc14b000aab3862
Facundo Alavar <facundo.alavar@enviopack.com>
2022-09-15 00:38:19 -0300

    Fix url icon TeamSeeder

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -38,22 +38,22 @@ class TeamSeeder extends Seeder
             ],
             [
                 'name'     => 'Inglaterra',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/gb-eng.png',
                 'group_id' => 2
             ],
             [
                 'name'     => 'Irán',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/ir.png',
                 'group_id' => 2
             ],
             [
                 'name'     => 'Estado Unidos',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/us.png',
                 'group_id' => 2
             ],
             [
                 'name'     => 'Gales',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/gb-wls.png',
                 'group_id' => 2
             ],
             [
@@ -63,77 +63,77 @@ class TeamSeeder extends Seeder
             ],
             [
                 'name'     => 'Arabia Saudita',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/sa.png',
                 'group_id' => 3
             ],
             [
                 'name'     => 'Mexico',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/mx.png',
                 'group_id' => 3
             ],
             [
                 'name'     => 'Polonia',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/pl.png',
                 'group_id' => 3
             ],
             [
                 'name'     => 'Francia',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/fr.png',
                 'group_id' => 4
             ],
             [
                 'name'     => 'Australia',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/au.png',
                 'group_id' => 4
             ],
             [
                 'name'     => 'Dinamarca',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/dk.png',
                 'group_id' => 4
             ],
             [
                 'name'     => 'Túnez',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/tn.png',
                 'group_id' => 4
             ],
             [
                 'name'     => 'España',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/es.png',
                 'group_id' => 5
             ],
             [
                 'name'     => 'Costa Rica',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/cr.png',
                 'group_id' => 5
             ],
             [
                 'name'     => 'Alemania',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/de.png',
                 'group_id' => 5
             ],
             [
                 'name'     => 'Japón',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/jp.png',
                 'group_id' => 5
             ],
             [
                 'name'     => 'Bélgica',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/be.png',
                 'group_id' => 6
             ],
             [
                 'name'     => 'Canadá',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/ca.png',
                 'group_id' => 6
             ],
             [
                 'name'     => 'Marruecos',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/ma.png',
                 'group_id' => 6
             ],
             [
                 'name'     => 'Croacia',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/hr.png',
                 'group_id' => 6
             ],
             [
@@ -143,37 +143,37 @@ class TeamSeeder extends Seeder
             ],
             [
                 'name'     => 'Serbia',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/rs.png',
                 'group_id' => 7
             ],
             [
                 'name'     => 'Suiza',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/ch.png',
                 'group_id' => 7
             ],
             [
                 'name'     => 'Camerún',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/cm.png',
                 'group_id' => 7
             ],
             [
                 'name'     => 'Portugal',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/pt.png',
                 'group_id' => 8
             ],
             [
                 'name'     => 'Ghana',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/gh.png',
                 'group_id' => 8
             ],
             [
                 'name'     => 'Uruguay',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/uy.png',
                 'group_id' => 8
             ],
             [
                 'name'     => 'Corea del Sur',
-                'icon'     => 'https://flagcdn.com/w20/qa.png',
+                'icon'     => 'https://flagcdn.com/w20/kr.png',
                 'group_id' => 8
             ],
         ];
