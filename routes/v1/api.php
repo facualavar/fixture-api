a21a1a844d448131215208b5c121608ddda89ac1
Facundo Alavar <facundo.alavar@enviopack.com>
2022-09-15 00:01:40 -0300

    fix routes group

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(GroupController::class)->group(function () {
     Route::get('/groups', 'index');
     Route::get('/groups/{id}', 'show');
-    Route::get('/groups/{id}/games', 'games');
-    Route::post('/groups/{id}/results', 'results');
+    Route::get('/groups/{id}/results', 'getResults');
+    Route::post('/groups/{id}/results', 'postResults');
 });
 Route::resource('games', GameController::class);
