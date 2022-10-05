e1ab3728b3757e46297892edb7636135d4ab34eb
Facundo Alavar <facundo.alavar@enviopack.com>
2022-10-05 00:45:26 -0300

    Change to clean architecture GroupController

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,71 +2,146 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\GroupService;
-use App\Models\Game;
-use App\Models\Group;
+use Fixture\Application\Services\Result\SaveResultsByUser;
+use Fixture\Domain\Group\Group;
+use Fixture\Domain\Team\Team;
+use Fixture\Infrastructure\Persistence\EloquentGameRepository;
+use Fixture\Infrastructure\Persistence\EloquentGroupRepository;
+use Fixture\Infrastructure\Persistence\EloquentMatchdayRepository;
+use Fixture\Infrastructure\Persistence\EloquentResultRepository;
+use Fixture\Infrastructure\Persistence\EloquentStageRepository;
+use Fixture\Infrastructure\Persistence\EloquentTeamRepository;
+use Fixture\Infrastructure\Persistence\EloquentUserRepository;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-use function PHPUnit\Framework\matches;
-
 class GroupController extends Controller
 {
+    private $stageRepository;
+    private $matchdayRepository;
+    private $groupRepository;
+    private $teamRepository;
+    private $resultRepository;
+    private $gameRepository;
+    private $userRepository;
+
+    public function __construct()
+    {
+        $this->stageRepository    = new EloquentStageRepository();
+        $this->matchdayRepository = new EloquentMatchdayRepository();
+        $this->teamRepository     = new EloquentTeamRepository();
+        $this->userRepository     = new EloquentUserRepository();
+        $this->groupRepository    = new EloquentGroupRepository($this->teamRepository);
+        $this->gameRepository     = new EloquentGameRepository($this->teamRepository, $this->groupRepository, $this->stageRepository, $this->matchdayRepository);
+        $this->resultRepository   = new EloquentResultRepository($this->gameRepository, $this->userRepository);
+    }
+
+    private function serializeTeam(Team $team): array
+    {
+        return [
+            'id'       => $team->getId(),
+            'name'     => $team->getName(),
+            'flag_img' => $team->getFlagImg(),
+        ];
+    }
+
+    private function serializeGroup(Group $group): array
+    {
+        $teamsData = [];
+        foreach ($group->getTeams() as $team) {
+            $teamsData[] = $this->serializeTeam($team);
+        }
+
+        return $response[] = [
+            'id'    => $group->getId(),
+            'name'  => $group->getName(),
+            'teams' => $teamsData,
+        ];
+    }
+
     public function index()
     {
-        $groups = Group::with('teams')->get();
-        return response()->json($groups, 200);
+        $groups = $this->groupRepository->all();
+
+        $response = [];
+        foreach ($groups as $group) {
+            $response[] = $this->serializeGroup($group);
+        }
+
+        return response()->json($response, 200);
     }
 
     public function show(int $id)
     {
-        $group = Group::with('teams')->find($id);
-        return response()->json($group, 200);
+        $group = $this->groupRepository->find($id);
+
+        if (!$group) throw new NotFoundHttpException("No se encontró el grupo");
+
+        $teams = $this->teamRepository->findByGroup($group);
+
+        $teamsData = [];
+        foreach ($teams as $team) {
+            $teamsData[] = [
+                'id'       => $team->getId(),
+                'name'     => $team->getName(),
+                'flag_img' => $team->getFlagImg(),
+            ];
+        }
+
+        $response = [
+            'id'    => $group->getId(),
+            'name'  => $group->getName(),
+            'teams' => $teamsData,
+        ];
+
+        return response()->json($response, 200);
     }
 
     public function getResults(int $id, Request $request)
     {
-        $user = $request->user();
+        $authUser = $request->user();
 
-        $games = Game::with('team1', 'team2', 'group', 'stage', 'matchday')->where('group_id', $id)->get();
+        $group = $this->groupRepository->find($id);
+        $user  = $this->userRepository->find($authUser->id);
 
-        $results = [];
-        foreach ($games as $game) {
+        if (!$group) throw new NotFoundHttpException("No se encontró el grupo");
+        if (!$user) throw new NotFoundHttpException("No se encontró el usuario");
 
-            $resultUser = [
-                'matchday'     => $game->matchday->name,
-                'name_team_1'  => $game->team1->name,
-                'flag_team_1'  => $game->team1->icon,
-                'goals_team_1' => null,
-                'name_team_2'  => $game->team2->name,
-                'flag_team_2'  => $game->team2->icon,
-                'goals_team_2' => null,
+        $games = $this->gameRepository->findByGroup($group);
+
+        $response = [];
+        foreach ($games as $game)
+        {
+            $result = $this->resultRepository->findByUserAndGame($user, $game);
+
+            $response[] = [
+                'stage'       => $game->getStage()->getName(),
+                'matchday'    => $game->getMatchday()->getName(),
+                'name_team_1' => $game->getTeam1()->getName(),
+                'flag_team_1' => $game->getTeam1()->getFlagImg(),
+                'name_team_2' => $game->getTeam2()->getName(),
+                'flag_team_2' => $game->getTeam2()->getFlagImg(),
+                'goals_team_1' => $result?->getGoalsTeam1(),
+                'goals_team_2' => $result?->getGoalsTeam2(),
             ];
-
-            foreach ($game->results as $result) {
-                if ($result->user_id == $user->id) {
-                    $resultUser['goals_team_1'] = $result->goals_team_1;
-                    $resultUser['goals_team_2'] = $result->goals_team_2;
-                }
-            }
-
-            $results[] = $resultUser;
         }
 
-        return response()->json($results, 200);
+        return response()->json($response, 200);
     }
 
     public function postResults(int $id, Request $request)
     {
         $results = $request->all();
-        $user    = $request->user();
+        $authUser = $request->user();
 
-        $group = Group::find($id);
+        $user  = $this->userRepository->find($authUser->id);
+        $group = $this->groupRepository->find($id);
 
-        if (!$group) throw new NotFoundHttpException();
+        if (!$group) throw new NotFoundHttpException("No se encontró el grupo");
+        if (!$user) throw new NotFoundHttpException("No se encontró el usuario");
 
-        $groupService = new GroupService($group);
-        $response     = $groupService->saveResults($user, $results);
+        $saveResults = new SaveResultsByUser($this->gameRepository, $this->resultRepository);
+        $response    = $saveResults->__invoke($user, $results);
 
         return response()->json($response, 200);
     }
